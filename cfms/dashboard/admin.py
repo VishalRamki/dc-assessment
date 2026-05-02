1b45ae06668a33517a822afecebee2ba23ee6825
VishalRamki <vishalramkissoon@live.com>
2026-05-02 09:44:49 -0400

    Add Migrations for Service Plans ; Attached User Profile (With Service Plan) only to Users under the Customer Group ; Added Seed data for service plan

--- cfms/dashboard/admin.py
+++ cfms/dashboard/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Complaint, ComplaintCategory, ComplaintStatus
+from .models import Complaint, ComplaintCategory, ComplaintStatus, ServicePlan, UserProfile
 
 # Register your models here.
 admin.site.register(Complaint)
 admin.site.register(ComplaintCategory)
 admin.site.register(ComplaintStatus)
+admin.site.register(ServicePlan)
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class CustomerInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "customer"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    def get_inlines(self, request, obj=None):
+        if obj:  # editing an existing user
+            if obj.groups.filter(name="Customer").exists():
+                return [CustomerInline]
+        return []
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- cfms/dashboard/models.py
+++ cfms/dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -42,3 +43,20 @@ class Complaint(models.Model):
     complaint_status_ref = models.ForeignKey(ComplaintStatus, on_delete=models.CASCADE)
 
     notes = models.ManyToManyField(Notes)
+
+class ServicePlan(models.Model):
+    name = models.CharField(max_length=128)
+    data = models.FloatField()
+    call = models.FloatField()
+    sms = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    service_plan_ref = models.ForeignKey(ServicePlan, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
+    
